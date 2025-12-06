3126cd7519a6d17dda69e5c8a39f2dbbca0d8286
Bhavik Patel <bhavikdpatel237@gmail.com>
2025-12-06 21:33:48 +0530

    initial commit

---
+++ src/test/java/com/api/securetrail/SecuretrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.securetrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuretrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
